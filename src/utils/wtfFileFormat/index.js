d61004a30fd158bd165a855734b1c9ad1060b5f5
sovlet <sovietxd@yandex.ru>
2021-07-14 03:37:47 +0500

    fix not working WtfFileFormat

--- src/utils/wtfFileFormat/index.js
+++ src/utils/wtfFileFormat/index.js
@@ -21,15 +21,15 @@ class WtfFileFormat {
         return parseInt(value, 10)
     }
     static parseContent(content) {
-        let data = {}
+        let data = new Object(null)
         let last = data
-        for(let line of raw.split('\n')) {
+        for(let line of content.split('\n')) {
             let [prefix, arg1, arg2] = lines[i].split(/\s+/)
             if(!prefix)
                 continue
             switch(prefix) {
                 case 'CAT':
-                    last = data[this.parseValue(arg1)] = {}
+                    last = data[this.parseValue(arg1)] = new Object(null)
                 case 'SET':
                     last[arg1] = this.parseValue(arg2)
             }
